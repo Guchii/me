4d422ef04ee3adcb5817e58c3ff45c60ed963eab
Guchii <71770229+Guchii@users.noreply.github.com>
2021-11-13 10:14:01 +0530

    using formingo.co for form submissions

--- pages/contact.js
+++ pages/contact.js
@@ -7,8 +7,8 @@ const Contact = () => {
     <>
       <div className="d-flex flex-column flex-lg-row justify-content-center align-items-center">
         <Image src={Naruto} className="me-lg-5 mb-3 mb-lg-0" alt="naruto" width={1280 * 0.3} height={720 * 0.3} />
-        <form className="card rounded p-3 shadow-lg" style={{ Height: '500px' }} name="contact" netlify>
-        {/* <input type="hidden" name="form-name" value="contact" /> */}
+        <form className="card rounded p-3 shadow-lg" style={{ Height: '500px' }} name="contact" action="https://www.formingo.co/submit/9ef6d3c8-29ca-4a0b-afa0-d539a0ff7c28"
+          method="POST">
           <div className="mb-3">
             <label htmlFor="name" className="form-label">Name</label>
             <input type="text" id="name" name="name" className="form-control" placeholder="guchiii srivastava" />
@@ -19,8 +19,8 @@ const Contact = () => {
           </div>
           <div className="mb-3">
             <label htmlFor="message" className="form-label">Message</label>
-            style={{ resize: "none" }}
             <textarea id="message" rows="3" name="message" className="form-control" placeholder="#include <iostream>"
+              style={{ resize: "none" }}
             />
           </div>
           <button type="submit" className="btn btn-primary fw-bold">Submit Response</button>
