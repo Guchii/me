b4c506e9828029f2261c2a8ab74a1994d1c83a5e
Shivom Srivastava <71770229+Guchii@users.noreply.github.com>
2021-12-13 15:48:00 +0530

    google verification

--- pages/_document.js
+++ pages/_document.js
@@ -11,11 +11,11 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,600;0,700;1,300;1,400;1,500;1,600&display=swap" rel="stylesheet" />
+          <meta name="google-site-verification" content="umuE18l7ZJfhDqnvm0EA1YE40Wtc3ikWpkxM5FyEUE8" />
         </Head>
         <body>
           <Main />
           <NextScript />
-
         </body>
       </Html>
     )
