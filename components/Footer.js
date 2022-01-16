b8a47dc9aae15524c2bd6807fd969b3a5a920526
Shivom <shvomsrivastava@gmail.com>
2022-01-16 20:17:48 +0530

    👔 add footer links

--- components/Footer.js
+++ components/Footer.js
@@ -1,4 +1,10 @@
-import { AiFillGithub, AiFillInstagram, AiFillMail } from "react-icons/ai";
+import {
+  AiFillGithub,
+  AiFillInstagram,
+  AiFillMail,
+  AiFillLinkedin,
+} from "react-icons/ai";
+import { SiMyanimelist } from "react-icons/si";
 import { FaSteam } from "react-icons/fa";
 import { useRouter } from "next/router";
 import { Flex, IconButton, Link } from "@chakra-ui/react";
@@ -17,10 +23,10 @@ const Footer = () => {
       >
         <Flex justifyContent={"space-between"} alignItems={"center"} gap={3}>
           <IconButton
-            aria-label="instagram"
+            aria-label="linkedin"
             size={"lg"}
-            icon={<AiFillInstagram />}
-            onClick={() => rd("https://instagram.com/guchi_xd/")}
+            icon={<AiFillLinkedin />}
+            onClick={() => rd("https://in.linkedin.com/in/shivom-srivastava")}
           />
           <IconButton
             aria-label="github"
@@ -29,16 +35,28 @@ const Footer = () => {
             onClick={() => rd("https://github.com/guchii")}
           />
           <IconButton
+            aria-label="mail"
+            size={"lg"}
+            icon={<AiFillMail />}
+            onClick={() => rd("mailto:shvomsrivastava@gmail.com")}
+          />
+          <IconButton
+            aria-label="instagram"
+            size={"lg"}
+            icon={<AiFillInstagram />}
+            onClick={() => rd("https://instagram.com/guchi_xd/")}
+          />
+          <IconButton
             aria-label="steam"
             size={"lg"}
             icon={<FaSteam />}
             onClick={() => rd("https://steamcommunity.com/id/guchixd/")}
           />
           <IconButton
-            aria-label="mail"
+            aria-label="myanimelist"
             size={"lg"}
-            icon={<AiFillMail />}
-            onClick={() => rd("mailto:shvomsrivastava@gmail.com")}
+            icon={<SiMyanimelist />}
+            onClick={() => rd("https://myanimelist.net/animelist/guchiii")}
           />
         </Flex>
         <Link href="https://github.com/guchii/guchii.github.io" isExternal>
