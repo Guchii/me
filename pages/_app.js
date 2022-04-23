af7141e6d29735c81c771f1f53f83b8464cdf076
Shivom <shvomsrivastava@gmail.com>
2022-04-23 11:02:52 +0530

    Remove resolution check

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import { ChakraProvider, Flex, useMediaQuery } from "@chakra-ui/react";
+import { ChakraProvider, Flex } from "@chakra-ui/react";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import MyTheme from "../styles/theme";
@@ -6,7 +6,6 @@ import "@fontsource/plus-jakarta-sans/400.css";
 import "@fontsource/montserrat/700.css";
 
 const MyApp = ({ Component, pageProps }) => {
-  const [isBigEnough] = useMediaQuery("(min-width: 320px)");
   const niceProps = {
     p: 6,
     width: "100%",
@@ -15,23 +14,16 @@ const MyApp = ({ Component, pageProps }) => {
   };
   return (
     <ChakraProvider theme={MyTheme} resetCSS>
-      {!isBigEnough ? (
-        <>
-          Your device&apos;s resolution may be very small. Please switch to
-          desktop mode if you are on a mobile browser.
-        </>
-      ) : (
-        <Flex
-          direction={"column"}
-          minHeight="100vh"
-          justifyContent="space-between"
-          overflow="hidden"
-        >
-          <Header />
-          <Component niceProps={niceProps} {...pageProps} />
-          <Footer />
-        </Flex>
-      )}
+      <Flex
+        direction={"column"}
+        minHeight="100vh"
+        justifyContent="space-between"
+        overflow="hidden"
+      >
+        <Header />
+        <Component niceProps={niceProps} {...pageProps} />
+        <Footer />
+      </Flex>
     </ChakraProvider>
   );
 };
