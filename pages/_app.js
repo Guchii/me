dac5af0415232756bcdb411f401a0d6d945ea0bd
Shivom <shvomsrivastava@gmail.com>
2022-04-23 13:51:44 +0530

    💫 route change transition

--- pages/_app.js
+++ pages/_app.js
@@ -1,32 +1,53 @@
-import { ChakraProvider } from "@chakra-ui/react";
+import { Box, ChakraProvider } from "@chakra-ui/react";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import MyTheme from "../styles/theme";
 import "@fontsource/plus-jakarta-sans/400.css";
 import "@fontsource/montserrat/700.css";
+import { motion } from "framer-motion";
 
-const MyApp = ({ Component, pageProps }) => {
+const ChakraBox = motion(Box);
+
+const MyApp = ({ Component, pageProps, router }) => {
   const niceProps = {
-    width: "100%",
     maxWidth: 1400,
     mx: "auto",
-    height: "calc(100vh - 88px - 96px)",
-    overflowY: "auto",
-    sx: {
-      "&::-webkit-scrollbar": {
-        width: "8px",
-        borderRadius: "15px",
-        backgroundColor: `rgba(0, 0, 0, 0.05)`,
-      },
-      "&::-webkit-scrollbar-thumb": {
-        backgroundColor: `rgba(0, 0, 0, 0.05)`,
-      },
-    },
+    height: "100%",
   };
   return (
     <ChakraProvider theme={MyTheme} resetCSS>
       <Header />
-      <Component niceProps={niceProps} {...pageProps} />
+      <ChakraBox
+        overflowY={"auto"}
+        height={"calc(100vh - 88px - 96px)"}
+        sx={{
+          "&::-webkit-scrollbar": {
+            width: "8px",
+            borderRadius: "15px",
+            backgroundColor: "rgba(0, 0, 0, 0.05)",
+          },
+          "&::-webkit-scrollbar-thumb": {
+            // backgroundColor: "rgba(0, 0, 0, 0.05)",
+            borderRadius: "15px",
+            backgroundColor: "purple.300",
+          },
+        }}
+        key={router.route}
+        initial={"empty"}
+        animate={"final"}
+        variants={{
+          empty: {
+            opacity: 0,
+            scale: 0.8,
+          },
+          final: {
+            opacity: 1,
+            scale: 1,
+          },
+        }}
+      >
+        <Component niceProps={niceProps} {...pageProps} />
+      </ChakraBox>
       <Footer />
     </ChakraProvider>
   );
