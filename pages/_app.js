52a90aa90237300cc3079402f50640891c0fae49
Shivom <shvomsrivastava@gmail.com>
2022-05-01 14:58:42 +0530

    cleanup

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import { Box, ChakraProvider } from "@chakra-ui/react";
 import { motion } from "framer-motion";
 
+import "../styles/globals.css";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import MyTheme from "../styles/theme";
@@ -22,17 +23,6 @@ const MyApp = ({ Component, pageProps, router }) => {
       <Header />
       <ChakraBox
         minHeight={"calc(100vh - 88px - 96px)"}
-        sx={{
-          "&::-webkit-scrollbar": {
-            width: "8px",
-            borderRadius: "15px",
-            backgroundColor: "rgba(0, 0, 0, 0.05)",
-          },
-          "&::-webkit-scrollbar-thumb": {
-            borderRadius: "15px",
-            backgroundColor: "purple.300",
-          },
-        }}
         key={router.route}
         initial={"empty"}
         animate={"final"}
