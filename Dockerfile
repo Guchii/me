49faab1592c8be3fb4c55ab735d8d63a40d6a00a
Shivom Srivastava <71770229+Guchii@users.noreply.github.com>
2022-12-13 18:02:22 +0530

    dipesh

--- Dockerfile
+++ Dockerfile
@@ -7,10 +7,11 @@ RUN pnpm export
 
 FROM node:16-alpine
 WORKDIR /srv
+ARG GITHUB_TOKEN
+ENV GITHUB_TOKEN=$GITHUB_TOKEN
 COPY --from=builder /app/out ./
 COPY --from=builder /app/fetch.mjs ./
+COPY app.json ./
 RUN chmod +x ./fetch.mjs
 RUN npm install -g serve zx
-# this script down below fetch latest pinned repositories
-RUN ./fetch.mjs  &
 CMD ["npx","serve","-l","tcp://0.0.0.0:3000"]
--- fetch.mjs
+++ fetch.mjs
@@ -4,39 +4,38 @@ const fs = require("fs")
 
 $.verbose = false
 
-while (true) {
-    await $`sleep 36000`
-    const response = await fetch('https://api.github.com/graphql', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            "Authorization": `Bearer ${process.env.GITHUB_TOKEN}`
-        },
-        body: JSON.stringify({
-            query: `{
-            user(login: "guchii") {
-                websiteUrl
-                bio
-                pinnedItems(first: 6, types: REPOSITORY) {
-                    nodes {
-                        ... on Repository {
-                            name
-                            description
-                            url
-                            homepageUrl
-                            createdAt
-                            updatedAt
-                        }
+const response = await fetch('https://api.github.com/graphql', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+        "Authorization": `Bearer ${process.env.GITHUB_TOKEN}`
+    },
+    body: JSON.stringify({
+        query: `{
+        user(login: "guchii") {
+            websiteUrl
+            bio
+            pinnedItems(first: 6, types: REPOSITORY) {
+                nodes {
+                    ... on Repository {
+                        name
+                        description
+                        url
+                        homepageUrl
+                        createdAt
+                        updatedAt
                     }
                 }
             }
-        }`})
-    });
+        }
+    }`})
+});
 
-    const data = await response.json()
+const data = await response.json()
 
-    if (!!data.data && data.data.user.pinnedItems.nodes.length !== 0) {
-        const final = { updated_at: new Date().toISOString(), projects: data.data.user.pinnedItems.nodes }
-        fs.writeFileSync("projects.json", JSON.stringify(final, null, 2))
-    }
+if (!!data.data && data.data.user.pinnedItems.nodes.length !== 0) {
+    const final = { updated_at: new Date().toISOString(), projects: data.data.user.pinnedItems.nodes }
+    fs.writeFileSync("projects.json", JSON.stringify(final, null, 2))
+    // append to a file in /tmp about the time when the task was completed
+    fs.appendFileSync("/tmp/fetch.log", `Completed at ${new Date().toISOString()}...`)
 }
